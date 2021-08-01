c192cba1473e8327b6946a0fc078eaf94e9dee51
mdave6 <85768375+mdave6@users.noreply.github.com>
2021-08-01 14:49:09 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_David
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello David, How are you doing today?");
+            Console.ReadLine();
+        }
+    }
+}
